7107385ea2e7d5ba21e0343cded29cb07fbbb628
Asteris <asteris.gian@gmail.com>
2020-09-21 13:18:39 +0300

    Page look updated.

--- src/main/webapp/app/modules/about/contact.tsx
+++ src/main/webapp/app/modules/about/contact.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Row, Col, Container } from 'reactstrap';
+import { Row, Col, Container, Input } from 'reactstrap';
 import { translate } from 'react-jhipster';
 
 export const Contact = () => (
   <Container>
-    <Row>
+    <Row className="my-5">
       <Col>
         <h1 className="text-center">
           {translate("about.contact.contact")}
@@ -12,30 +12,43 @@ export const Contact = () => (
       </Col>
     </Row>
     <Row>
-      <Col className="text-center">{translate("about.contact.phone")}</Col>
-      <Col className="text-center">{translate("about.contact.email")}</Col>
-      <Col className="text-center">{translate("about.contact.address")}</Col>
+      <Col className="text-center"><span className="font-weight-bold">{translate("about.contact.phone")}</span><br/><br/>
+        <span className="text-info">210 7491613-614</span></Col>
+      <Col className="text-center"><span className="font-weight-bold">{translate("about.contact.email")}</span><br/><br/>
+        <span className="text-info">something@ekke.gr</span></Col>
+      <Col className="text-center"><span className="font-weight-bold">{translate("about.contact.address")}</span><br/><br/>
+        <span className="text-info">Κρατίνου 9 και Αθηνάς, Πλατεία Κοτζιά, 105 52 Αθήνα</span></Col>
     </Row>
-    <Row>
-      <Col>
-        <h1 className="text-center">
-          {translate("about.contact.feedback")}
+    <Row className="my-5">
+      <Col  className="text-center">
+        <h1>
+          {translate("about.contact.feedback.title")}
         </h1>
+        <br/>
+        <p>
+          {translate("about.contact.feedback.subtitle")}
+        </p>
       </Col>
     </Row>
-    <Row>
-      <Col>
-        <input/>
+    <Row className="my-3">
+      <Col className="text-center" sm="12" md={{ size: 4, offset: 4 }}>
+        <span>{translate("about.contact.feedback.fields.fullName")}</span>
+        <br/>
+        <Input/>
       </Col>
     </Row>
-    <Row>
-      <Col>
-        <input/>
+    <Row className="my-3">
+      <Col className="text-center" sm="12" md={{ size: 4, offset: 4 }}>
+        <span>{translate("about.contact.feedback.fields.email")}</span>
+        <br/>
+        <Input/>
       </Col>
     </Row>
-    <Row>
-      <Col>
-        <input/>
+    <Row className="my-3">
+      <Col className="text-center" sm="12" md={{ size: 6, offset: 3 }}>
+        <span>{translate("about.contact.feedback.fields.comment")}</span>
+        <br/>
+        <Input type="textarea" />
       </Col>
     </Row>
   </Container>
