95e47bb5fb1cce1a26bd219aad15ec5c2a65913f
Asteris <asteris.gian@gmail.com>
2020-09-22 12:38:31 +0300

    Pagination added for articles on fact checking sub menus.

--- src/main/webapp/app/modules/fact-checking/sub-menus.tsx
+++ src/main/webapp/app/modules/fact-checking/sub-menus.tsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { connect } from 'react-redux';
 import { IRootState } from 'app/shared/reducers';
 import { RouteComponentProps } from 'react-router-dom';
 import { Row, Container, Col } from 'reactstrap';
-import {Translate} from "react-jhipster";
+import {getSortState, JhiItemCount, JhiPagination, Translate} from "react-jhipster";
+import { getArticlesByCategoryName } from 'app/entities/article/article.reducer';
+import {overridePaginationStateWithQueryParams} from "app/shared/util/entity-utils";
+import {ITEMS_PER_PAGE} from "app/shared/util/pagination.constants";
 
 export interface ISubMenusProps extends StateProps, DispatchProps, RouteComponentProps<{ id: string }> {}
 
 export const SubMenus = (props: ISubMenusProps) => {
+  const [paginationState, setPaginationState] = useState(
+    overridePaginationStateWithQueryParams(getSortState(props.location, ITEMS_PER_PAGE), props.location.search)
+  );
 
+  useEffect(() => {
+    props.getArticlesByCategoryName(props.match.params.id);
+  }, []);
+
+  const getAllEntities = () => {
+    props.getArticlesByCategoryName(props.match.params.id, paginationState.activePage - 1, paginationState.itemsPerPage, `${paginationState.sort},${paginationState.order}`);
+  }
+
+  const sortEntities = () => {
+    getAllEntities();
+    const endURL = `?page=${paginationState.activePage}&sort=${paginationState.sort},${paginationState.order}`;
+    if (props.location.search !== endURL) {
+      props.history.push(`${props.location.pathname}${endURL}`);
+    }
+  };
+
+  useEffect(() => {
+    sortEntities();
+  }, [paginationState.activePage, paginationState.order, paginationState.sort]);
+
+  useEffect(() => {
+    const params = new URLSearchParams(props.location.search);
+    const page = params.get('page');
+    const sort = params.get('sort');
+    if (page && sort) {
+      const sortSplit = sort.split(',');
+      setPaginationState({
+        ...paginationState,
+        activePage: +page,
+        sort: sortSplit[0],
+        order: sortSplit[1],
+      });
+    }
+  }, [props.location.search]);
+
+  const handlePagination = currentPage =>
+    setPaginationState({
+      ...paginationState,
+      activePage: currentPage,
+    });
+
+  const { articlesByCategory } = props;
 
   return (
     <Container>
@@ -21,13 +69,51 @@ export const SubMenus = (props: ISubMenusProps) => {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate posuere lorem eu sodales. Aliquam vel justo nulla. Ut finibus dolor ac placerat molestie. Praesent eget ipsum metus. Sed eget lectus convallis nisl sodales consequat. Aenean interdum urna dolor, ultricies fermentum dui iaculis sed. Morbi non lorem porttitor, ullamcorper nisi laoreet, pellentesque nibh. Pellentesque nec aliquet mauris. Phasellus eu tortor sagittis justo rutrum lobortis sed ut risus. Nullam ipsum libero, ultricies et ligula ac, placerat rutrum lorem. Sed urna urna, vestibulum eget purus eget, interdum suscipit nisl. Cras a sapien libero. Mauris magna risus, congue eu molestie in, luctus id lorem. Donec eget tempor lorem. Praesent varius vitae est non tempus. Donec condimentum purus ex, tempus hendrerit massa dictum et.</p>
         </Col>
       </Row>
+      {articlesByCategory.map(article => (
+        <Row className="my-5" key={article.id}>
+          <Col>
+            {article.previewImage
+              ? <img src={`data:${article.previewImageContentType};base64,${article.previewImage}`} alt="previewImage" style={{ display: 'block', margin: 'auto' }} />
+              : null
+            }
+          </Col>
+          <Col className="border-left">
+            <h2 className="text-center">{article.previewTitle}/{article.id}</h2>
+            <p className="mt-5">{article.previewText}</p>
+          </Col>
+        </Row>
+      ))}
+      {props.totalItems ? (
+        <div className={articlesByCategory && articlesByCategory.length > 0 ? '' : 'd-none'}>
+          <Row className="justify-content-center">
+            <JhiItemCount page={paginationState.activePage} total={props.totalItems} itemsPerPage={paginationState.itemsPerPage} i18nEnabled />
+          </Row>
+          <Row className="justify-content-center">
+            <JhiPagination
+              activePage={paginationState.activePage}
+              onSelect={handlePagination}
+              maxButtons={5}
+              itemsPerPage={paginationState.itemsPerPage}
+              totalItems={props.totalItems}
+            />
+          </Row>
+        </div>
+      ) : (
+        ''
+      )}
     </Container>
   );
 };
 
-const mapStateToProps = (storeState: IRootState) => ({});
+const mapStateToProps = (storeState: IRootState) => ({
+  articlesByCategory: storeState.article.entities,
+  loading: storeState.article.loading,
+  totalItems: storeState.article.totalItems
+});
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  getArticlesByCategoryName
+};
 
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
