51246b30ab1c03c9d118794e6df80c0764dc110f
Asteris <asteris.gian@gmail.com>
2020-10-07 13:23:08 +0300

    Removed Ethics and ThirdParty Links.

--- src/main/webapp/app/modules/fact-checking/fact-checking.tsx
+++ src/main/webapp/app/modules/fact-checking/fact-checking.tsx
@@ -4,7 +4,6 @@ import { IRootState } from 'app/shared/reducers';
 import { Translate, translate } from 'react-jhipster';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { Row, Col, Input, Button, Container } from 'reactstrap';
-import {THIRD_PARTY_LINK} from "app/config/constants";
 import { reset } from "app/modules/fact-checking/fact-checking.reducer";
 
 export interface IFactCheckingProps extends StateProps, DispatchProps, RouteComponentProps<{ url: string }> {}
@@ -29,14 +28,12 @@ export const FactChecking = (props: IFactCheckingProps) => {
             <Translate contentKey="fact-checking.page-guidelines.main" /><br/>
             <ul style={{ listStyleType: 'none', fontWeight: 'bold' }}>
               <li><Translate contentKey="fact-checking.page-guidelines.guide-1" /></li>
-              <li><Translate contentKey="fact-checking.page-guidelines.guide-2" /></li>
-              <li><Translate contentKey="fact-checking.page-guidelines.guide-3" /></li>
             </ul>
           </p>
         </Col>
       </Row>
       <Row  className="my-5">
-        <Col className="text-center border-bottom pb-5" sm="12" md={{ size: 6, offset: 3 }}>
+        <Col className="text-center pb-5" sm="12" md={{ size: 6, offset: 3 }}>
           <Input
             value={statement}
             onChange={handleStatementChange}
@@ -46,29 +43,6 @@ export const FactChecking = (props: IFactCheckingProps) => {
           <Button color="primary" disabled={statement === ''} tag={Link} to={`${props.match.url}/result/${statement.split(' ').join('_')}`}>
             <Translate contentKey="fact-checking.check.button"/>
           </Button>
-        </Col>
-      </Row>
-      <Row className="my-5">
-        <Col className="text-center" sm="12" md={{ size: 6, offset: 3 }}>
-          <h2>
-            <Link className="text-primary" to={`${props.match.url}/ethics`}>
-              <Translate contentKey="fact-checking.ethics.main" />
-            </Link>
-          </h2>
-        </Col>
-      </Row>
-      <Row className="my-5">
-        <Col className="text-center pt-5 border-top" sm="12" md={{ size: 6, offset: 3 }}>
-          <h2><Translate contentKey="fact-checking.third-party" /></h2><br/><br/>
-          <ul style={{ listStyleType: 'none', fontWeight: 'bold' }}>
-            {
-              THIRD_PARTY_LINK.map((item, index) => (
-                <li key={index}>
-                  <a href={item.link} target="_blank"  rel="noopener noreferrer">{item.name}</a>
-                </li>
-              ))
-            }
-          </ul>
         </Col>
       </Row>
     </Container>
--- src/main/webapp/app/modules/fact-checking/index.tsx
+++ src/main/webapp/app/modules/fact-checking/index.tsx
@@ -3,7 +3,6 @@ import { Switch } from 'react-router-dom';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 import FactChecking from 'app/modules/fact-checking/fact-checking';
 import SubMenus from 'app/modules/fact-checking/sub-menus';
-import Ethics from 'app/modules/fact-checking/ethics';
 import FactCheckResult from "app/modules/fact-checking/fact-checking-result";
 
 
@@ -12,7 +11,6 @@ const Routes = ({ match }) => (
     <Switch>
       <ErrorBoundaryRoute path={`${match.url}/result/:id`} component={FactCheckResult} />
       <ErrorBoundaryRoute path={`${match.url}/sub-menu/:id`} component={SubMenus} />
-      <ErrorBoundaryRoute path={`${match.url}/ethics`} exact component={Ethics} />
       <ErrorBoundaryRoute path={`${match.url}`} component={FactChecking} />
     </Switch>
   </div>
