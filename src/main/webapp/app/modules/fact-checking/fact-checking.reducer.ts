813bbe13c7fbf3f1031bca0738daf233355b1e46
Asteris <asteris.gian@gmail.com>
2021-01-22 16:41:40 +0200

    Action for GetTaskStatus created.

--- src/main/webapp/app/modules/fact-checking/fact-checking.reducer.ts
+++ src/main/webapp/app/modules/fact-checking/fact-checking.reducer.ts
@@ -5,8 +5,9 @@ import { IStatement } from 'app/shared/model/statement.model';
 export const ACTION_TYPES = {
   SET_FACT: 'fact-checking/SET_FACT',
   SET_URLS: 'fact-checking/SET_URLS',
-  ANALYZE_STATEMENT: 'fact-checking/ANALYZE_STATEMENT',
-  TRAIN: 'fact-checking/TRAIN',
+  ANALYZE_TASK: 'fact-checking/ANALYZE_TASK',
+  TRAIN_TASK: 'fact-checking/TRAIN_TASK',
+  GET_TASK_STATUS: 'fact-checking/GET_TASK_STATUS',
   RESET: 'fact-checking/RESET',
 };
 
@@ -19,6 +20,8 @@ const initialState = {
   training: false,
   trainingLoading: false,
   trainingResponse: null,
+  taskStatusLoading: false,
+  taskStatusResponse: null,
 };
 
 export type FactCheckingState = Readonly<typeof initialState>;
@@ -27,40 +30,57 @@ export type FactCheckingState = Readonly<typeof initialState>;
 
 export default (state: FactCheckingState = initialState, action): FactCheckingState => {
   switch (action.type) {
-    case REQUEST(ACTION_TYPES.ANALYZE_STATEMENT):
+    case REQUEST(ACTION_TYPES.ANALYZE_TASK):
       return {
         ...state,
         analyzeLoading: true,
       };
-    case FAILURE(ACTION_TYPES.ANALYZE_STATEMENT):
+    case FAILURE(ACTION_TYPES.ANALYZE_TASK):
       return {
         ...state,
         analyzeLoading: false,
-        errorMessage: action.payload.data,
+        errorMessage: action.payload,
       };
-    case SUCCESS(ACTION_TYPES.ANALYZE_STATEMENT):
+    case SUCCESS(ACTION_TYPES.ANALYZE_TASK):
       return {
         ...state,
         analyzeLoading: false,
         analyzeResponse: action.payload.data,
       };
-    case REQUEST(ACTION_TYPES.TRAIN):
+    case REQUEST(ACTION_TYPES.TRAIN_TASK):
       return {
         ...state,
         trainingLoading: true,
       };
-    case FAILURE(ACTION_TYPES.TRAIN):
+    case FAILURE(ACTION_TYPES.TRAIN_TASK):
       return {
         ...state,
         trainingLoading: false,
-        errorMessage: action.payload.data,
+        errorMessage: action.payload,
       };
-    case SUCCESS(ACTION_TYPES.TRAIN):
+    case SUCCESS(ACTION_TYPES.TRAIN_TASK):
       return {
         ...state,
         trainingLoading: false,
         training: true,
         trainingResponse: action.payload.data,
+      };
+    case REQUEST(ACTION_TYPES.GET_TASK_STATUS):
+      return {
+        ...state,
+        taskStatusLoading: true,
+      };
+    case FAILURE(ACTION_TYPES.GET_TASK_STATUS):
+      return {
+        ...state,
+        taskStatusLoading: false,
+        errorMessage: action.payload,
+      };
+    case SUCCESS(ACTION_TYPES.GET_TASK_STATUS):
+      return {
+        ...state,
+        taskStatusLoading: false,
+        taskStatusResponse: action.payload.data,
       };
     case ACTION_TYPES.SET_FACT:
       return {
@@ -89,7 +109,7 @@ export const analyzeStatement = (statement: IStatement) => (dispatch, getState) 
   const { inProduction } = getState().applicationProfile;
   const requestUrl = `${inProduction ? '/ml' : pythonUrl}/analyze`;
   return dispatch({
-    type: ACTION_TYPES.ANALYZE_STATEMENT,
+    type: ACTION_TYPES.ANALYZE_TASK,
     payload: axios.post(requestUrl, statement),
   });
 };
@@ -98,8 +118,17 @@ export const trainModel = () => (dispatch, getState) => {
   const { inProduction } = getState().applicationProfile;
   const requestUrl = `${inProduction ? '/ml' : pythonUrl}/train`;
   return dispatch({
-    type: ACTION_TYPES.TRAIN,
+    type: ACTION_TYPES.TRAIN_TASK,
     payload: axios.post(requestUrl),
+  });
+};
+
+export const getTaskStatus = id => (dispatch, getState) => {
+  const { inProduction } = getState().applicationProfile;
+  const requestUrl = `${inProduction ? '/ml' : pythonUrl}/task-status/${id}`;
+  return dispatch({
+    type: ACTION_TYPES.GET_TASK_STATUS,
+    payload: axios.get(requestUrl),
   });
 };
 
