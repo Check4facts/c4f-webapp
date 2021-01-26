829c8a353d48a0f409731b1e80d488ae6f475ad4
Asteris <asteris.gian@gmail.com>
2021-01-26 13:14:45 +0200

    Collapse for model results PLUS some UI changes.

--- src/main/webapp/app/modules/fact-checking/fact-checking-results.tsx
+++ src/main/webapp/app/modules/fact-checking/fact-checking-results.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import './fact-checking.scss'
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { IRootState } from 'app/shared/reducers';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { Row, Col, Table, Button, Container, Spinner } from 'reactstrap';
+import { Row, Col, Table, Button, Container, Spinner, Collapse } from 'reactstrap';
 import { setFact } from 'app/modules/fact-checking/fact-checking.reducer';
 import { getEntity as getStatement, updateEntity as updateStatement } from 'app/entities/statement/statement.reducer';
 import { getStatementSourcesByStatement } from 'app/entities/statement-source/statement-source.reducer';
@@ -15,6 +16,9 @@ import moment from "moment";
 export interface IFactCheckingResultsProps extends StateProps, DispatchProps, RouteComponentProps<{ id: string }> {}
 
 export const FactCheckingResults = (props: IFactCheckingResultsProps) => {
+
+  const [emotionCollapse, setEmotionCollapse] = useState(false);
+  const [restCollapse, setRestCollapse] = useState(false);
 
   useEffect(() => {
     props.setFact(props.match.params.id);
@@ -65,6 +69,9 @@ export const FactCheckingResults = (props: IFactCheckingResultsProps) => {
           <Col>
             <h4>{translate("check4FactsApp.statement.statementDate")}</h4>
           </Col>
+          <Col>
+            <h4>{translate("check4FactsApp.statement.registrationDate")}</h4>
+          </Col>
         </Row>
         <Row className="text-center my-3">
           <Col>
@@ -72,6 +79,9 @@ export const FactCheckingResults = (props: IFactCheckingResultsProps) => {
           </Col>
           <Col>
             <h5>{moment.locale(currentLocale) && moment(statement.statementDate).format("LL")}</h5>
+          </Col>
+          <Col>
+            <h5>{moment.locale(currentLocale) && moment(statement.registrationDate).format("LL")}</h5>
           </Col>
         </Row>
         <Row className="text-center my-3 text-info">
@@ -96,135 +106,141 @@ export const FactCheckingResults = (props: IFactCheckingResultsProps) => {
         </Row>
         <Row className="text-center my-3 text-info">
           <Col>
-            <h4>{translate("fact-checking.results.model.emotions")}</h4>
+            <h4 className="result-collapse" onClick={() => setEmotionCollapse(!emotionCollapse)}>{translate("fact-checking.results.model.emotions")}</h4>
           </Col>
         </Row>
-        <Row className="text-center my-3 text-primary border-bottom">
-          <Col><h5>Πεδίο</h5></Col>
-          <Col><h5>Θυμός</h5></Col>
-          <Col><h5>Απέχθεια</h5></Col>
-          <Col><h5>Φόβος</h5></Col>
-          <Col><h5>Χαρά</h5></Col>
-          <Col><h5>Λύπη</h5></Col>
-          <Col><h5>Έκπληξη</h5></Col>
-        </Row>
-        <Row className="text-center">
-          <Col><p>Δήλωση</p></Col>
-          <Col><p>{((featureStatement.sEmotionAnger[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((featureStatement.sEmotionDisgust[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((featureStatement.sEmotionFear[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((featureStatement.sEmotionHappiness[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((featureStatement.sEmotionSadness[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((featureStatement.sEmotionSurprise[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
-        </Row>
-        <Row className="text-center">
-          <Col><p>Τίτλοι</p></Col>
-          <Col><p>{(featureStatement.rTitleEmotionAnger[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rTitleEmotionDisgust[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rTitleEmotionFear[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rTitleEmotionHappiness[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rTitleEmotionSadness[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rTitleEmotionSurprise[3] * 100).toFixed(2)}%</p></Col>
-        </Row>
-        <Row className="text-center">
-          <Col><p>Κείμενα</p></Col>
-          <Col><p>{(featureStatement.rBodyEmotionAnger[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rBodyEmotionDisgust[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rBodyEmotionFear[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rBodyEmotionHappiness[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rBodyEmotionSadness[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rBodyEmotionSurprise[3] * 100).toFixed(2)}%</p></Col>
-        </Row>
-        <Row className="text-center">
-          <Col><p>Αντιπροσωπ/τερες Παράγραφοι</p></Col>
-          <Col><p>{(featureStatement.rSimParEmotionAnger[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimParEmotionDisgust[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimParEmotionFear[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimParEmotionHappiness[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimParEmotionSadness[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimParEmotionSurprise[3] * 100).toFixed(2)}%</p></Col>
-        </Row>
-        <Row className="text-center border-bottom">
-          <Col><p>Αντιπροσωπ/τερες Προτάσεις</p></Col>
-          <Col><p>{(featureStatement.rSimSentEmotionAnger[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimSentEmotionDisgust[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimSentEmotionFear[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimSentEmotionHappiness[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimSentEmotionSadness[3] * 100).toFixed(2)}%</p></Col>
-          <Col><p>{(featureStatement.rSimSentEmotionSurprise[3] * 100).toFixed(2)}%</p></Col>
-        </Row>
+        <Collapse isOpen={emotionCollapse}>
+          <Row className="text-center my-3 text-primary border-bottom">
+            <Col><h5>Πεδίο</h5></Col>
+            <Col><h5>Θυμός</h5></Col>
+            <Col><h5>Απέχθεια</h5></Col>
+            <Col><h5>Φόβος</h5></Col>
+            <Col><h5>Χαρά</h5></Col>
+            <Col><h5>Λύπη</h5></Col>
+            <Col><h5>Έκπληξη</h5></Col>
+          </Row>
+          <Row className="text-center">
+            <Col><p>Δήλωση</p></Col>
+            <Col><p>{((featureStatement.sEmotionAnger[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((featureStatement.sEmotionDisgust[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((featureStatement.sEmotionFear[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((featureStatement.sEmotionHappiness[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((featureStatement.sEmotionSadness[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((featureStatement.sEmotionSurprise[3] / featureStatement.sFertileTerms) * 100).toFixed(2)}%</p></Col>
+          </Row>
+          <Row className="text-center">
+            <Col><p>Τίτλοι</p></Col>
+            <Col><p>{(featureStatement.rTitleEmotionAnger[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rTitleEmotionDisgust[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rTitleEmotionFear[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rTitleEmotionHappiness[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rTitleEmotionSadness[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rTitleEmotionSurprise[3] * 100).toFixed(2)}%</p></Col>
+          </Row>
+          <Row className="text-center">
+            <Col><p>Κείμενα</p></Col>
+            <Col><p>{(featureStatement.rBodyEmotionAnger[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rBodyEmotionDisgust[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rBodyEmotionFear[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rBodyEmotionHappiness[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rBodyEmotionSadness[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rBodyEmotionSurprise[3] * 100).toFixed(2)}%</p></Col>
+          </Row>
+          <Row className="text-center">
+            <Col><p>Αντιπροσωπ/τερες Παράγραφοι</p></Col>
+            <Col><p>{(featureStatement.rSimParEmotionAnger[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimParEmotionDisgust[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimParEmotionFear[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimParEmotionHappiness[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimParEmotionSadness[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimParEmotionSurprise[3] * 100).toFixed(2)}%</p></Col>
+          </Row>
+          <Row className="text-center border-bottom">
+            <Col><p>Αντιπροσωπ/τερες Προτάσεις</p></Col>
+            <Col><p>{(featureStatement.rSimSentEmotionAnger[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimSentEmotionDisgust[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimSentEmotionFear[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimSentEmotionHappiness[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimSentEmotionSadness[3] * 100).toFixed(2)}%</p></Col>
+            <Col><p>{(featureStatement.rSimSentEmotionSurprise[3] * 100).toFixed(2)}%</p></Col>
+          </Row>
+        </Collapse>
         <Row className="text-center mt-5 mb-3 text-info">
           <Col>
-            <h4>{translate("fact-checking.results.model.rest")}</h4>
+            <h4 className="result-collapse" onClick={() => setRestCollapse(!restCollapse)}>{translate("fact-checking.results.model.rest")}</h4>
           </Col>
         </Row>
-        <Row className="text-center my-3 text-primary border-bottom">
-          <Col><h5>Πεδίο</h5></Col>
-          <Col><h5>Αντικειμενικότητα</h5></Col>
-          <Col><h5>Συναισθηματική Ένταση</h5></Col>
-          <Col><h5>Ομοιότητα με Δήλωση</h5></Col>
-        </Row>
-        <Row className="text-center">
-          <Col><p>Δήλωση</p></Col>
-          <Col><p>{((1 - featureStatement.sSubjectivity) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((1 - featureStatement.sSentiment) * 100).toFixed(2)}%</p></Col>
-          <Col><p>-</p></Col>
-        </Row>
-        <Row className="text-center">
-          <Col><p>Τίτλοι</p></Col>
-          <Col><p>{((1 - featureStatement.rTitleSubjectivity) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((1 - featureStatement.rTitleSentiment) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((1 - featureStatement.rTitleSimilarity) * 100).toFixed(2)}%</p></Col>
-        </Row>
-        <Row className="text-center">
-          <Col><p>Κείμενα</p></Col>
-          <Col><p>{((1 - featureStatement.rBodySubjectivity) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((1 - featureStatement.rBodySentiment) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((1 - featureStatement.rBodySimilarity) * 100).toFixed(2)}%</p></Col>
-        </Row>
-        <Row className="text-center">
-          <Col><p>Αντιπροσωπ/τερες Παράγραφοι</p></Col>
-          <Col><p>{((1 - featureStatement.rSimParSubjectivity) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((1 - featureStatement.rSimParSentiment) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((1 - featureStatement.rSimParSimilarity) * 100).toFixed(2)}%</p></Col>
-        </Row>
-        <Row className="text-center border-bottom">
-          <Col><p>Αντιπροσωπ/τερες Προτάσεις</p></Col>
-          <Col><p>{((1 - featureStatement.rSimSentSubjectivity) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((1 - featureStatement.rSimSentSentiment) * 100).toFixed(2)}%</p></Col>
-          <Col><p>{((1 - featureStatement.rSimSentSimilarity) * 100).toFixed(2)}%</p></Col>
-        </Row>
-        <Row className="my-3">
+        <Collapse isOpen={restCollapse}>
+          <Row className="text-center my-3 text-primary border-bottom">
+            <Col><h5>Πεδίο</h5></Col>
+            <Col><h5>Αντικειμενικότητα</h5></Col>
+            <Col><h5>Συναισθηματική Ένταση</h5></Col>
+            <Col><h5>Ομοιότητα με Δήλωση</h5></Col>
+          </Row>
+          <Row className="text-center">
+            <Col><p>Δήλωση</p></Col>
+            <Col><p>{((1 - featureStatement.sSubjectivity) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((1 - featureStatement.sSentiment) * 100).toFixed(2)}%</p></Col>
+            <Col><p>-</p></Col>
+          </Row>
+          <Row className="text-center">
+            <Col><p>Τίτλοι</p></Col>
+            <Col><p>{((1 - featureStatement.rTitleSubjectivity) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((1 - featureStatement.rTitleSentiment) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((1 - featureStatement.rTitleSimilarity) * 100).toFixed(2)}%</p></Col>
+          </Row>
+          <Row className="text-center">
+            <Col><p>Κείμενα</p></Col>
+            <Col><p>{((1 - featureStatement.rBodySubjectivity) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((1 - featureStatement.rBodySentiment) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((1 - featureStatement.rBodySimilarity) * 100).toFixed(2)}%</p></Col>
+          </Row>
+          <Row className="text-center">
+            <Col><p>Αντιπροσωπ/τερες Παράγραφοι</p></Col>
+            <Col><p>{((1 - featureStatement.rSimParSubjectivity) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((1 - featureStatement.rSimParSentiment) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((1 - featureStatement.rSimParSimilarity) * 100).toFixed(2)}%</p></Col>
+          </Row>
+          <Row className="text-center border-bottom">
+            <Col><p>Αντιπροσωπ/τερες Προτάσεις</p></Col>
+            <Col><p>{((1 - featureStatement.rSimSentSubjectivity) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((1 - featureStatement.rSimSentSentiment) * 100).toFixed(2)}%</p></Col>
+            <Col><p>{((1 - featureStatement.rSimSentSimilarity) * 100).toFixed(2)}%</p></Col>
+          </Row>
+          <Row className="my-3">
+            <Col>
+              <ul className="text-muted">
+                <li>Οι τιμές που αφορούν το πεδίο «Δήλωση» αναφέρονται στο ποσοστό των όρων που ταυτοποιήθηκαν με το εκάστοτε χαρακτηριστικό.</li>
+                <li>Tα πεδία «Τίτλοι», «Κείμενα», «Αντιπροσωπ/τερες Παράγραφοι» και «Αντιπροσωπ/τερες Προτάσεις» αναφέρονται στο σύνολο των αντίστοιχων πεδίων που προκύπτουν από τις πηγές που ανακτήθηκαν. Οι τιμές των πεδίων αυτών αναφέρονται στο ποσοστό των προτάσεων που ταυτοποιήθηκαν με το εκάστοτε χαρακτηριστικό.</li>
+              </ul>
+            </Col>
+          </Row>
+        </Collapse>
+        <Row className="text-center py-3">
           <Col>
-            <ul className="text-muted">
-              <li>Οι τιμές που αφορούν το πεδίο «Δήλωση» αναφέρονται στο ποσοστό των όρων που ταυτοποιήθηκαν με το εκάστοτε χαρακτηριστικό.</li>
-              <li>Tα πεδία «Τίτλοι», «Κείμενα», «Αντιπροσωπ/τερες Παράγραφοι» και «Αντιπροσωπ/τερες Προτάσεις» αναφέρονται στο σύνολο των αντίστοιχων πεδίων που προκύπτουν από τις πηγές που ανακτήθηκαν. Οι τιμές των πεδίων αυτών αναφέρονται στο ποσοστό των προτάσεων που ταυτοποιήθηκαν με το εκάστοτε χαρακτηριστικό.</li>
-            </ul>
+            <h4 className="text-info">Απόφαση Ελεγκτή</h4>
+          </Col>
+          <Col>
+            <h4 className={statement.factCheckerLabel ? 'text-success' : 'text-danger'}>{statement.factCheckerLabel ? 'Αληθής' : 'Ψευδής'}</h4>
+          </Col>
+          <Col className="d-flex justify-content-center">
+            <Button
+              color="info"
+              onClick={toggleFactCheckerLabel}
+            >
+              Αλλαγή
+            </Button>
+          </Col>
+        </Row>
+        <Row className="text-center">
+          <Col>
+            <p>Με βάση τα παραπάνω στοιχεία μπορείτε να αλλάξετε την κατάσταση της δήλωσης αυτής</p>
           </Col>
         </Row>
         <Row className="my-3">
           <Col className="d-flex justify-content-center" md={{ size: 4, offset: 4 }}>
-            <Button tag={Link} to="/article/new" color="info">
+            <Button tag={Link} to="/article/new" color="primary">
               {translate("fact-checking.analyze.action.createArticle")}
-            </Button>
-          </Col>
-        </Row>
-        <Row className="mt-5 text-center">
-          <Col>
-            <h5>Με βάση τα παραπάνω στοιχεία μπορείτε να αλλάξετε την κατάσταση της δήλωσης αυτής</h5>
-            <p>
-              Η τωρινή της κατάσταση φαίνεται στο παρακάτω κουμπί.<br/>
-              Πατείστε πάνω σε αυτό για να γίνει αλλαγή στην αντίθετη.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="d-flex justify-content-center">
-            <Button
-              color={statement.factCheckerLabel ? 'success' : 'danger'}
-              onClick={toggleFactCheckerLabel}
-            >
-              {statement.factCheckerLabel ? 'Αληθής' : 'Ψευδής'}
             </Button>
           </Col>
         </Row>
