548a960842ebf3f5c46ae6731f46821b3547a582
Konstantinos Kozanis <kon.kozanis@gmail.com>
2022-06-30 16:50:16 +0300

    name changes & fix links in social

--- src/main/webapp/app/shared/layout/menus/social.tsx
+++ src/main/webapp/app/shared/layout/menus/social.tsx
@@ -4,17 +4,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const Social = () => (
   <ul className="social-media">
     <li>
-      <a href="https://www.twitter.com">
+      <a href="https://twitter.com/Check4F">
         <FontAwesomeIcon icon={['fab', 'twitter']}/>
       </a>
     </li>
     <li>
-      <a href="https://www.facebook.com">
+      <a href="https://www.facebook.com/Check4Facts">
         <FontAwesomeIcon icon={['fab', 'facebook']} />
       </a>
     </li>
     <li>
-      <a href="https://www.instagram.com">
+      <a href="https://www.instagram.com/check4facts/">
         <FontAwesomeIcon icon={['fab', 'instagram']} />
       </a>
     </li>
