1a924cf28b06d63b7d57c706d5c9e925b4c10628
stavr <stavrosmrl@gmail.com>
2022-11-14 17:28:01 +0200

    Logo and text changes

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -7,7 +7,7 @@ export const Brand = () => (
   <NavbarBrand tag={Link} to="/" className="brand-logo">
     <span className="brand-title">
       {/* <Translate contentKey="global.title">Check4facts</Translate> */}
-      <img src='/content/images/CHECK 4 FACTS MAIN BRAND WHITE 2.png' width="300px" alt="Check4Facts"/>
+      <img src='/content/images/check4facts_science_logo.jpg' width="300px" alt="Check4Facts"/>
     </span>
   </NavbarBrand>
 );
