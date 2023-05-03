1b262f49e68cd17dddda09081a7512fb4e493f3a
Konstantinos Kozanis <kon.kozanis@gmail.com>
2023-05-03 09:21:47 +0300

    changes to about/project

--- src/main/webapp/app/modules/dissemination/dissemination.tsx
+++ src/main/webapp/app/modules/dissemination/dissemination.tsx
@@ -66,6 +66,12 @@ export const Dissemination = () => {
       content: (
         <div>
           <div className="embed-responsive embed-responsive-16by9">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/G5V8bzNGlE8"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+          </div>
+          <div className="embed-responsive embed-responsive-16by9 mt-3">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/I4IpJRz5xxQ"
                     title="YouTube video player" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
