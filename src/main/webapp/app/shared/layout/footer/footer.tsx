2e805eaf416a75e924165169d79c23d950104952
Konstantinos Kozanis <kon.kozanis@gmail.com>
2024-05-27 17:24:21 +0300

    Update funding information in project and footer components

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -48,16 +48,15 @@ const Footer = (props: IFooterProps) => (
           </div>
         </Col>
         <Col className='mt-4 mt-md-0' lg="5">
-          <p className="mb-2">Το έργο χρηματοδοτήθηκε από το <a href="http://www.elidek.gr/en/homepage/">Ελληνικό Ίδρυμα Έρευνας και Καινοτομίας</a> (grant agreement HFRI-FM17-2283).</p>
-          <p className="font-weight-medium">
-            <a href={props.currentLocale === "el" ? 'https://check4facts.gr/files/c4f-privacy-policy-gr.pdf' : 
-              'https://check4facts.gr/files/c4f-privacy-policy-eng.pdf'} target="_blank" rel="noopener noreferrer" className="text-dark d-flex align-items-center w-auto">
-              {translate('global.privacy-policy')}
-            </a>
-          </p>
+          <p className="mb-2">Το έργο χρηματοδοτήθηκε από το <a href="http://www.elidek.gr/en/homepage/">ΕΛΙΔΕΚ</a> (grant agreement HFRI-FM17-2283) και από το Εθνικό Σχεδίο Ανάκαμψης και Ανθεκτικότητας Ελλάδα 2.0 με τη χρηματοδότηση της Ευρωπαϊκής Ένωσης – NextGenerationEU</p>
+          <Row className="justify-content-md-center justify-content-lg-start d-sm-flex" style={{display: "flex", justifyContent: "center", columnGap: 10}}>
           <a href='http://www.elidek.gr/' target="_blank" rel="noopener noreferrer" className="text-dark d-flex align-items-center">
-            <img src="../../../content/images/ELIDEK.png" alt="ELIDEK logo" height="50"/>
+            <img src="../../../content/images/ELIDEK.png" alt="ELIDEK logo" height="40"/>
           </a>
+          <a href='https://greece20.gov.gr/' target="_blank" rel="noopener noreferrer" className="text-dark d-flex align-items-center">
+            <img src="../../../content/images/Greece-2.0_NextGeneration_gr_1.jpg" alt="Greece 2.0 logo" height="40"/>
+          </a>
+          </Row>
         </Col>
         <Col className='mt-4 mt-md-0' lg="2" style={{display: "flex", flexDirection: "column"}}>
           <Row className="d-flex justify-content-center">
@@ -74,7 +73,11 @@ const Footer = (props: IFooterProps) => (
         </Col>
       </Row>
       <Row className="justify-content-center">
-        <p className="font-weight-medium" style={{fontSize: 12}}>© Copyright 2020 <a href="https://www.check4facts.gr/" className="text-dark">check4facts.gr</a></p>
+        <p className="font-weight-medium d-flex flex-wrap" style={{fontSize: 12, columnGap: 8}}>© Copyright 2020 <a href="https://www.check4facts.gr/" className="text-dark">check4facts.gr</a>
+            <a href={props.currentLocale === "el" ? 'https://check4facts.gr/files/c4f-privacy-policy-gr.pdf' : 
+              'https://check4facts.gr/files/c4f-privacy-policy-eng.pdf'} target="_blank" rel="noopener noreferrer" className="text-dark d-flex align-items-center w-auto">
+              {translate('global.privacy-policy')}
+            </a></p>
       </Row>
     </Container>
   </footer>
