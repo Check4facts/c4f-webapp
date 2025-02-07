e3bdc6f0a8d7b58c113c97b051ca8271a0d2b6b1
Asteris Giannoudis <asteris.gian@gmail.com>
2025-02-07 16:41:55 +0200

    UI changes for Summarization and SummarizationDisplay components.

--- src/main/webapp/app/modules/summarization/summarization.tsx
+++ src/main/webapp/app/modules/summarization/summarization.tsx
@@ -64,31 +64,35 @@ const Summarization = (props: ISummarization) => {
         {tracking ? (
           <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <Col md={{ size: 6, offset: 0 }}>
-              <p style={{ textAlign: 'center' }}>Διαδικασία Δημιουργίας Περίληψης</p>
+              <p style={{ textAlign: 'center' }}>
+                <Translate contentKey="check4FactsApp.summarization.progress" />
+              </p>
               <Progress animated color="info" value={100} />
             </Col>
           </Row>
         ) : (
           <>
             <Row>
-              {summary && (
-                <Col className="summary" md={{ size: 9, offset: 1 }}>
-                  <div id="toolbar-container" />
-                  <CKEditor
-                    editor={DecoupledEditor}
-                    onChange={formOnChange}
-                    data={summary}
-                    config={{ toolbar: ['bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'] }}
-                    onInit={editor => {
-                      // Add the toolbar to the container
-                      const toolbarContainer = document.querySelector('#toolbar-container');
-                      toolbarContainer.appendChild(editor.ui.view.toolbar.element);
-                    }}
-                    ref={editorRef}
-                  />
-                </Col>
-              )}
-              <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Col className="summary" md={{ size: 10, offset: 1 }} style={{ padding: '0' }}>
+                <div id="toolbar-container" />
+                <CKEditor
+                  editor={DecoupledEditor}
+                  onChange={formOnChange}
+                  data={
+                    summary
+                      ? summary
+                      : '<p style="text-align:center;"><span style="color:hsl(0,0%,60%);"><i>Δημιουργήστε αυτόματα την περίληψη</i></span></p>'
+                  }
+                  config={{ toolbar: ['bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'] }}
+                  onInit={editor => {
+                    // Add the toolbar to the container
+                    const toolbarContainer = document.querySelector('#toolbar-container');
+                    toolbarContainer.appendChild(editor.ui.view.toolbar.element);
+                  }}
+                  ref={editorRef}
+                />
+              </Col>
+              <Col style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'flexStart', padding: '0' }}>
                 <Button
                   id="generate-summary"
                   color="primary"
@@ -101,9 +105,11 @@ const Summarization = (props: ISummarization) => {
                     open: true,
                   })}
                 >
-                  <FontAwesomeIcon icon="cloud" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" width="16" height="16">
+                    <path d="M152.1 38.2c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 113C-2.3 103.6-2.3 88.4 7 79s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zm0 160c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 273c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zM224 96c0-17.7 14.3-32 32-32l224 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-224 0c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32l224 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-224 0c-17.7 0-32-14.3-32-32zM160 416c0-17.7 14.3-32 32-32l288 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-288 0c-17.7 0-32-14.3-32-32zM48 368a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                  </svg>
                 </Button>
-                <Tooltip target="generate-summary" placement={summary ? 'right' : 'top'} toggle={toggleTooltip} isOpen={tooltipOpen}>
+                <Tooltip target="generate-summary" placement="top" toggle={toggleTooltip} isOpen={tooltipOpen}>
                   Περίληψη με ΑΙ
                 </Tooltip>
               </Col>
