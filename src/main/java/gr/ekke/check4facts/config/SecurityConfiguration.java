703653dda259f44ec5b5f7d5e0c8d065122f76ec
Asteris Giannoudis <asteris.gian@gmail.com>
2025-02-18 14:14:40 +0200

    Updated CSP for images.

--- src/main/java/gr/ekke/check4facts/config/SecurityConfiguration.java
+++ src/main/java/gr/ekke/check4facts/config/SecurityConfiguration.java
@@ -65,7 +65,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .headers()
             // Container implemetation
             // .contentSecurityPolicy("default-src 'self'; connect-src 'self' https://check4facts.gr/ml https://check4facts.gr; frame-src 'self' youtube.com https://www.youtube.com/ data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com data:")
-            .contentSecurityPolicy("default-src 'self'; frame-src 'self' youtube.com https://www.youtube.com/  img-src * data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com data:")
+            .contentSecurityPolicy("default-src 'self'; frame-src 'self' youtube.com https://www.youtube.com/ data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; img-src * data:; font-src 'self' https://fonts.gstatic.com data:")
         .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
         .and()
