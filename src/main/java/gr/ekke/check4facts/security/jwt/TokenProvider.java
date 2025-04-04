c27217baaa34c252a236b5e9b2094685a5b94dcd
Asteris Giannoudis <asteris.gian@gmail.com>
2025-04-04 15:49:17 +0300

    Check for Base64-encoded JWT secret is done first.

--- src/main/java/gr/ekke/check4facts/security/jwt/TokenProvider.java
+++ src/main/java/gr/ekke/check4facts/security/jwt/TokenProvider.java
@@ -43,14 +43,15 @@ public class TokenProvider {
     @PostConstruct
     public void init() {
         byte[] keyBytes;
-        String secret = jHipsterProperties.getSecurity().getAuthentication().getJwt().getSecret();
+        String secret = jHipsterProperties.getSecurity().getAuthentication().getJwt().getBase64Secret();
         if (!StringUtils.isEmpty(secret)) {
+            log.debug("Using a Base64-encoded JWT secret key");
+            keyBytes = Decoders.BASE64.decode(secret);
+        } else {
             log.warn("Warning: the JWT key used is not Base64-encoded. " +
                 "We recommend using the `jhipster.security.authentication.jwt.base64-secret` key for optimum security.");
+            secret = jHipsterProperties.getSecurity().getAuthentication().getJwt().getSecret();
             keyBytes = secret.getBytes(StandardCharsets.UTF_8);
-        } else {
-            log.debug("Using a Base64-encoded JWT secret key");
-            keyBytes = Decoders.BASE64.decode(jHipsterProperties.getSecurity().getAuthentication().getJwt().getBase64Secret());
         }
         this.key = Keys.hmacShaKeyFor(keyBytes);
         this.tokenValidityInMilliseconds =
